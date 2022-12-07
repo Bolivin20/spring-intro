c0299c1825dbb500aaba2b6987d78551e029aed2
Nikodem Zbaraza <nikozet@interia.pl>
2022-12-07 18:44:05 +0100

    added new class Random..

---
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import java.util.Random;
+
+public class RandomTextMessageService implements MessageService{
+
+    private String[] messages = {"Wiadomosc 1", "Wiadomosc 2", "Wiadomosc 3", "Wiadomosc 4", "Wiadomosc 5", "Wiadomosc 6", "Wiadomosc 7", "Wiadomosc 8", "Wiadomosc 9", "Wiadomosc 10"};
+
+    @Override
+    public String getMessage() {
+        int random = new Random().nextInt(messages.length);
+        return messages[random];
+    }
+}
