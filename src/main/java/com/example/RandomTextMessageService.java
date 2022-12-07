456c8a1c5a44b24b5f3f12d45464280408a90b0e
Nikodem Zbaraza <nikozet@interia.pl>
2022-12-07 18:57:58 +0100

    added annotations

--- src/main/java/com/example/RandomTextMessageService.java
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -2,6 +2,9 @@ package com.example;
 
 import java.util.Random;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class RandomTextMessageService implements MessageService{
 
     private String[] messages = {"Wiadomosc 1", "Wiadomosc 2", "Wiadomosc 3", "Wiadomosc 4", "Wiadomosc 5", "Wiadomosc 6", "Wiadomosc 7", "Wiadomosc 8", "Wiadomosc 9", "Wiadomosc 10"};
